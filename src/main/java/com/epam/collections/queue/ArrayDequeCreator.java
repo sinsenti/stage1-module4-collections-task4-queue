e029332c0ee567dac1c60d5bb6a8d9b881ac527b
sinsenti <sinsenti73@gmail.com>
2025-07-07 12:49:07 +0300

    Auto-push: 2025-07-07 12:49:07

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -5,41 +5,43 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
+
   public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
-    ArrayDeque<Integer> deque = new ArrayDeque<>();
+    ArrayDeque<Integer> resultDeque = new ArrayDeque<>();
 
     for (int i = 0; i < 2; i++) {
-      deque.addLast(firstQueue.poll());
+      resultDeque.addLast(firstQueue.poll());
     }
     for (int i = 0; i < 2; i++) {
-      deque.addLast(secondQueue.poll());
+      resultDeque.addLast(secondQueue.poll());
     }
 
-    boolean firstPlayersTurn = true;
+    boolean isFirstPlayersTurn = true;
+
     while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
-      if (firstPlayersTurn) {
-        if (!deque.isEmpty()) {
-          firstQueue.add(deque.pollFirst());
+      if (isFirstPlayersTurn) {
+        if (!resultDeque.isEmpty()) {
+          firstQueue.add(resultDeque.pollFirst());
         }
         for (int i = 0; i < 2; i++) {
           if (!firstQueue.isEmpty()) {
-            deque.addLast(firstQueue.poll());
-          } else if (firstQueue.isEmpty() && secondQueue.isEmpty()) {
-            break;
+            resultDeque.addLast(firstQueue.poll());
           }
         }
       } else {
-        if (!deque.isEmpty()) {
-          secondQueue.add(deque.pollFirst());
+        if (!resultDeque.isEmpty()) {
+          secondQueue.add(resultDeque.pollFirst());
         }
         for (int i = 0; i < 2; i++) {
           if (!secondQueue.isEmpty()) {
-            deque.addLast(secondQueue.poll());
+            resultDeque.addLast(secondQueue.poll());
           }
         }
       }
-      firstPlayersTurn = !firstPlayersTurn;
+      isFirstPlayersTurn = !isFirstPlayersTurn;
     }
-    return deque;
+
+    return resultDeque;
   }
+
 }
